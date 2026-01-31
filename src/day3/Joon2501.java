963dc2e21b82d2c084f83692cad87d9fdca92995
mi-yeon777 <web01214@gmail.com>
2026-02-01 02:19:04 +0900

    2026.1.31

--- src/day3/Joon2501.java
+++ src/day3/Joon2501.java
@@ -2,6 +2,8 @@ package day3;
 
 import java.util.Scanner;
 
+//문제: https://www.acmicpc.net/problem/2501
+
 public class Joon2501 {
     public static void main(String[] args) {
 
@@ -11,19 +13,18 @@ public class Joon2501 {
 
         // nextInt()
         // -> 입력된 정수를 하나 읽어옴
-        int N = sc.nextInt();   // 문제: 약수를 구할 대상 숫자N
+        int N = sc.nextInt();   // 문제: 약수를 구할 대상 숫자N(나뉘어지는 수)
         int K = sc.nextInt();   // 몇 번째 약수를 찾을지
 
         // 약수의 개수를 세기 위한 변수
         int count = 0;
 
-        // for문
+        // for문: 정해진 횟수나 조건에 따라 코드 블록을 반복 실행하는 제어문// for(초기식; 조건식; 증감식)
         // i를 1부터 N까지 1씩 증가시키며 반복
         for (int i = 1; i <= N; i++) {
 
-            // if문
-            // N % i == 0
-            // → N을 i로 나눴을 때 나머지가 0이면 i는 N의 약수
+            // if문: if (조건식) { ... } - 조건이 참일 때 블록 실행.
+            // N을 i로 나눴을 때 나머지가 0이면 i는 N의 약수
             if (N % i == 0) {
 
                 // 약수를 하나 찾았으므로 개수 증가
